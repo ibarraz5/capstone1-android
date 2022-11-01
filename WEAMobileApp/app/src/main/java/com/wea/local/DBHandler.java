41db813094d589401f96084179be7c153f1d4003
A klein <akendo21@gmail.com>
2022-10-31 23:01:41 -0500

    Adding message storage support

--- WEAMobileApp/app/src/main/java/com/wea/local/DBHandler.java
+++ WEAMobileApp/app/src/main/java/com/wea/local/DBHandler.java
@@ -17,15 +17,38 @@ public class DBHandler extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String query = "CREATE TABLE " + CMAC_ALERT_TABLE_NAME + " ("
-                + CMAC_MESSAGE_NO_COL + " TEXT, "
+                + CMAC_MESSAGE_NO_COL + " TEXT PRIMARY KEY NOT NULL, "
                 + CMAC_CAP_ID_COL + " TEXT, "
                 + CMAC_SENDER_COL + " TEXT, "
                 + CMAC_DATE_TIME_COL + " DATETIME, "
                 + CMAC_MESSAGE_TYPE_COL + " TEXT)";
+
+        db.execSQL(query);
+    }
+
+    public void addNewCMACAlert(String cmacMessageNo, String cmacCapId, String cmacSender, String dateTime, String cmacMessageType) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(CMAC_MESSAGE_NO_COL, cmacMessageNo);
+        values.put(CMAC_CAP_ID_COL, cmacCapId);
+        values.put(CMAC_SENDER_COL, cmacSender);
+        values.put(CMAC_DATE_TIME_COL, dateTime);
+        values.put(CMAC_MESSAGE_TYPE_COL, cmacMessageType);
+
+        db.insert(CMAC_ALERT_TABLE_NAME, null, values);
+
+        db.close();
     }
 
     public DBHandler(Context context){
         super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 
 }
