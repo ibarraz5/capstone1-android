c5fee93b5c896e2da800f5fbc50b1b4c54ebceda
dargall91 <dargall91@comcast.net>
2022-11-08 22:38:11 -0500

    Fix part of parser
    Simple XML is not simple and I miss Jackson XML

--- WEAMobileApp/app/src/main/java/com/wea/local/model/CMACMessageAlertInfo.java
+++ WEAMobileApp/app/src/main/java/com/wea/local/model/CMACMessageAlertInfo.java
@@ -9,6 +9,67 @@ import java.util.List;
 
 @Root(strict = false)
 public class CMACMessageAlertInfo {
+    //I miss fasterxml.jackson...
+    @Root(strict = false)
+    private static class CMACMessageAlertArea {
+        @Element(name = "CMAC_area_description")
+        @Path("CMAC_Alert_Text")
+        private String areaDescription;
+
+        @Element(name = "CMAC_polygon")
+        @Path("CMAC_Alert_Text")
+        private String polygon;
+
+        @Element(name = "CMAC_circle")
+        @Path("CMAC_Alert_Text")
+        private String circle;
+
+        @ElementList(name = "CMAC_cmas_geocode")
+        @Path("CMAC_Alert_Text")
+        private List<String> geocodeList;
+
+        public String getAreaDescription() {
+            return areaDescription;
+        }
+
+        public String getPolygon() {
+            return polygon;
+        }
+
+        public String getCircle() {
+            return circle;
+        }
+
+        public List<String> getGeocodeList() {
+            return geocodeList;
+        }
+    }
+
+    @Root(strict = false)
+    private static class CMACMessageAlertText {
+        @Element(name = "CMAC_text_language")
+        @Path("CMAC_Alert_Text")
+        private String language;
+        @Element(name = "CMAC_short_text_alert_message")
+        @Path("CMAC_Alert_Text")
+        private String shortMessage;
+        @Element(name = "CMAC_long_text_alert_message")
+        @Path("CMAC_Alert_Text")
+        private String longMessage;
+
+        public String getLanguage() {
+            return language;
+        }
+    }
+
+    @Element(name = "CMAC_category")
+    private String category;
+    @Element(name = "CMAC_severity")
+    private String severity;
+    @Element(name = "CMAC_urgency")
+    private String urgency;
+    @Element(name = "CMAC_certainty")
+    private String certainty;
     @Element(name = "CMAC_expires_date_time")
     private String expires;
 
@@ -16,8 +77,12 @@ public class CMACMessageAlertInfo {
     private String senderName;
 
     @ElementList(name = "CMAC_Alert_Area")
-    @Path("CMAC_Alert_Attributes/CMAC_alert_info/CMAC_Alert_Area")
+    ///@Path("CMAC_alert_info")
     private List<CMACMessageAlertArea> alertAreaList;
+
+    @ElementList(name = "CMAC_Alert_Text")
+    //@Path("CMAC_alert_info")
+    private List<CMACMessageAlertText> alertTextList;
 
     public String getExpires() {
         return expires;
@@ -27,7 +92,15 @@ public class CMACMessageAlertInfo {
         return senderName;
     }
 
-    public List<CMACMessageAlertArea> getAlertArea() {
+    public List<CMACMessageAlertArea> getAlertAreaList() {
         return alertAreaList;
+    }
+
+    public List<CMACMessageAlertText> getAlertTextList() {
+        return alertTextList;
+    }
+
+    public String getLanguage(int index) {
+        return alertTextList.get(index).getLanguage();
     }
 }
